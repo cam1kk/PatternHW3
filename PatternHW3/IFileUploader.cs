61d4fe62ad430772448e20c0f3116e8b4616da2c
Babae_me80 <Babae_me80@STHQ0127-06>
2024-06-09 19:47:46 +0400

    Add project files.

---
+++ PatternHW3/IFileUploader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PatternHW3
+{
+    public interface IFileUploader
+    {
+        void UploadFile(string filePath);
+    }
+
+    public class GoogleDriveUploader : IFileUploader
+    {
+        public void UploadFile(string filePath)
+        {
+            MessageBox.Show($"Uploading {filePath} to Google Drive...");
+            MessageBox.Show($"File {filePath} uploaded successfully to Google Drive.");
+        }
+    }
+
+    public class DropboxUploaderDecorator : IFileUploader
+    {
+        private readonly IFileUploader _fileUploader;
+
+        public DropboxUploaderDecorator(IFileUploader fileUploader)
+        {
+            _fileUploader = fileUploader;
+        }
+
+        public void UploadFile(string filePath)
+        {
+            MessageBox.Show($"Uploading {filePath} to Dropbox...");
+            MessageBox.Show($"File {filePath} uploaded successfully to Dropbox.");
+        }
+    }
+}
---
+++ PatternHW3/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PatternHW3
+{
+    public partial class MainWindow : Window
+    {
+        private IFileUploader _fileUploader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _fileUploader = new GoogleDriveUploader();
+        }
+
+        private void UploadFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = @"C:\example.txt";
+            _fileUploader.UploadFile(filePath);
+        }
+
+        private void UseDropboxCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (useDropboxCheckbox.IsChecked == true)
+            {
+                _fileUploader = new DropboxUploaderDecorator(_fileUploader);
+            }
+            else
+            {
+                _fileUploader = new GoogleDriveUploader();
+            }
+        }
+    }
+}
